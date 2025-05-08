1205d29af956567e44645b3c9a10021253c0e154
SI Aluno <si_aluno@ds.feb.br>
2025-05-08 19:56:34 -0300

    ex matrizes feito

--- matriz.py
+++ matriz.py
@@ -6,7 +6,7 @@ def calcular_matriz(matriz):
             print(matriz[0][0])
         case 2:
             print(
-                matriz[0][0] * matriz[1][1]
+                  matriz[0][0] * matriz[1][1]
                 - matriz[0][1] * matriz[1][0]
             )
         case 3:
@@ -21,9 +21,9 @@ def calcular_matriz(matriz):
         case _:
             print('Erro')
 
-matriz1 = [[1]]
-matriz2 = [[1, 2], [4, 3]]
-matriz3 = [[7, 2, 1], [4, 3, 9], [8, 2, 1]]
+matriz1 = [[2]]
+matriz2 = [[2, 5], [7, 3]]
+matriz3 = [[1, 2, 3], [2, 5, 6], [2, 5, 8]]
 
 calcular_matriz(matriz1)
 calcular_matriz(matriz2)
